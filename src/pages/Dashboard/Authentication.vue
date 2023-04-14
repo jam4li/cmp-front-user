6088746bf12d47724592754d2cbd714ea9d01645
jam4li <ziigm4@gmail.com>
2023-04-14 10:58:41 +0100

    Modify Authentication template

--- src/pages/Dashboard/Authentication.vue
+++ src/pages/Dashboard/Authentication.vue
@@ -1,55 +1,77 @@
 <template>
   <div class="md-layout">
-    <div class="md-layout-item md-size-100 md-xsmall-size-100 mx-auto">
-      <simple-wizard>
-        <template slot="header">
-          <h3 class="title">{{ $t("authentication.title") }}</h3>
-          <h5 class="category">
-            {{ $t("authentication.subtitle") }}
-          </h5>
+    <div class="md-layout-item md-size-100 md-medium-size-100">
+      <tabs
+        :tab-name="['Email', 'Pending', 'Accepted', 'Rejected']"
+        color-button="info"
+      >
+        <h4 class="title" slot="header-title">
+          {{ $t("authentication.title") }} -
+          <small class="description">{{ $t("authentication.subtitle") }}</small>
+        </h4>
+
+        <!-- here you can add your content for tab-content -->
+        <template slot="tab-pane-1">
+          <form class="form-horizontal" @submit.prevent="submitForm">
+            <div class="md-layout">
+              <label class="md-layout-item md-size-25 md-form-label">
+                Email
+              </label>
+              <div class="md-layout-item md-size-75">
+                <md-field>
+                  <label>Enter Email</label>
+                  <md-input v-model="email" type="email"></md-input>
+                </md-field>
+              </div>
+            </div>
+
+            <md-card-actions md-alignment="left">
+              <md-button type="submit" class="md-success">Submit</md-button>
+            </md-card-actions>
+          </form>
         </template>
 
-        <wizard-tab>
-          <template slot="label"> Email </template>
-          <first-step ref="step1"></first-step>
-        </wizard-tab>
+        <template slot="tab-pane-2"> </template>
 
-        <wizard-tab>
-          <template slot="label"> Pending </template>
-          <second-step ref="step2"></second-step>
-        </wizard-tab>
-
-        <wizard-tab>
-          <template slot="label"> Accepted </template>
-          <second-step ref="step2"></second-step>
-        </wizard-tab>
-
-        <wizard-tab>
-          <template slot="label"> Rejected </template>
-          <second-step ref="step2"></second-step>
-        </wizard-tab>
-      </simple-wizard>
+        <template slot="tab-pane-3"> </template>
+      </tabs>
     </div>
   </div>
 </template>
 <script>
-import FirstStep from "./Forms/Wizard/FirstStep.vue";
-import SecondStep from "./Forms/Wizard/SecondStep.vue";
-import Swal from "sweetalert2";
-import { SimpleWizard, WizardTab } from "@/components";
+import api from "@/api.js";
+import { Tabs } from "@/components";
 
 export default {
+  components: {
+    Tabs,
+  },
   data() {
     return {
-      wizardModel: {},
+      email: "",
     };
   },
-  components: {
-    FirstStep,
-    SecondStep,
-    SimpleWizard,
-    WizardTab,
+  methods: {
+    submitForm() {
+      api
+        .post("api/v1/exchange/create/", {
+          parent_email: this.email,
+        })
+        .then((response) => {
+          if (response.data.success) {
+            this.$emit("emailSubmitted", response.data);
+            alert(response.data.message);
+            // Reset the form
+            this.email = "";
+          } else {
+            alert(response.data);
+          }
+        })
+        .catch((error) => {
+          console.error("Error submitting form:", error);
+          alert("Error submitting form. Please try again.");
+        });
+    },
   },
-  methods: {},
 };
 </script>
