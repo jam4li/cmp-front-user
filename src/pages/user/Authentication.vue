263faf265ff9929fd518ab0161ce48eb13a92e07
jam4li <ziigm4@gmail.com>
2023-04-23 10:11:27 +0100

    Add handling of user status during authentication
    - Implementing logic to check user status (Accepted, Pending, Rejected) in the authentication process
    - Displaying appropriate messages based on the user's status

--- src/pages/user/Authentication.vue
+++ src/pages/user/Authentication.vue
@@ -12,23 +12,46 @@
 
         <!-- here you can add your content for tab-content -->
         <template slot="tab-pane-1">
-          <form class="form-horizontal" @submit.prevent="submitForm">
-            <div class="md-layout">
-              <label class="md-layout-item md-size-25 md-form-label">
-                Email
-              </label>
-              <div class="md-layout-item md-size-75">
-                <md-field>
-                  <label>Enter Email</label>
-                  <md-input v-model="email" type="email"></md-input>
-                </md-field>
-              </div>
+          <div v-if="status === 'accepted'">
+            <h6>
+              <md-icon>verified_user</md-icon>
+              Congratulations! Your request has been accepted by
+              {{ parentInfo.parent }}
+            </h6>
+          </div>
+          <div v-else-if="status === 'pending'">
+            <h6>
+              <md-icon>pending</md-icon>
+              Your request is awaiting approval from
+              {{ parentInfo.parent }}
+            </h6>
+          </div>
+          <div v-else>
+            <div v-if="status === 'rejected'">
+              <h6>
+                <md-icon>error</md-icon>
+                Unfortunately, your request has been rejected by
+                {{ parentInfo.parent }}
+              </h6>
             </div>
+            <form class="form-horizontal" @submit.prevent="submitForm">
+              <div class="md-layout">
+                <label class="md-layout-item md-size-25 md-form-label">
+                  Email
+                </label>
+                <div class="md-layout-item md-size-75">
+                  <md-field>
+                    <label>Enter Email</label>
+                    <md-input v-model="email" type="email"></md-input>
+                  </md-field>
+                </div>
+              </div>
 
-            <md-card-actions md-alignment="left">
-              <md-button type="submit" class="md-success">Submit</md-button>
-            </md-card-actions>
-          </form>
+              <md-card-actions md-alignment="left">
+                <md-button type="submit" class="md-success">Submit</md-button>
+              </md-card-actions>
+            </form>
+          </div>
         </template>
 
         <template slot="tab-pane-2"> </template>
@@ -51,9 +74,33 @@ export default {
   data() {
     return {
       email: "",
+      status: null,
+      parentInfo: null,
     };
   },
+  created() {
+    this.fetchParentInfo();
+  },
   methods: {
+    async fetchParentInfo() {
+      try {
+        const response = await api.get("/api/v1/exchange/user/detail/");
+        if (response.data.success) {
+          this.parentInfo = response.data.data;
+          if (this.parentInfo.status) {
+            this.status = this.parentInfo.status;
+          }
+        } else {
+          this.notifyVue(response.data.error, "danger", "error_outline");
+        }
+      } catch (error) {
+        this.notifyVue(
+          "An error occurred while fetching data",
+          "danger",
+          "error_outline"
+        );
+      }
+    },
     submitForm() {
       api
         .post("api/v1/exchange/user/create/", {
