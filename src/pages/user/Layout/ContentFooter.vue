0e1551fb1c33b28183c72976786adc0950673f1d
jam4li <ziigm4@gmail.com>
2023-07-06 22:23:41 +0330

    Modify CMP to CME in title tag

--- src/pages/user/Layout/ContentFooter.vue
+++ src/pages/user/Layout/ContentFooter.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <div class="copyright text-center">
         &copy; {{ new Date().getFullYear() }}
-        <a href="https://cloudminepro.com/" target="_blank">CloudMine Pro.</a
-        >All Rights Reserved
+        <a href="https://cloudminepro.com/" target="_blank">CME.</a>All Rights
+        Reserved
       </div>
     </div>
   </footer>
