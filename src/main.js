844690842f60d5f2f5773d9f65cd3f66da504d89
jam4li <ziigm4@gmail.com>
2023-07-25 17:14:06 +0330

    Modify path of routes.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-// =========================================================
-// * Vue Material Dashboard PRO - v1.5.2
-// =========================================================
-//
-// * Product Page: https://www.creative-tim.com/product/vue-material-dashboard-pro
-// * Copyright 2022 Creative Tim (https://www.creative-tim.com)
-//
-// * Coded by Creative Tim
-//
-// =========================================================
-//
-// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 import Vue from "vue";
 import VueRouter from "vue-router";
 import DashboardPlugin from "./material-dashboard";
@@ -25,7 +12,7 @@ import VueAwesomeSwiper from "vue-awesome-swiper";
 import "swiper/swiper-bundle.min.css";
 
 // router setup
-import router from "./routes/routes";
+import router from "./routes";
 
 // plugin setup
 Vue.use(VueRouter);
